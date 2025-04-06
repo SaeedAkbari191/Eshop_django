4f98bcaacb4375f559cf923f7c7ddd68808d1894
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-04-06 18:38:15 +0430

    Ecommerce

--- account_module/views.py
+++ account_module/views.py
@@ -1,8 +1,10 @@
-from logging import raiseExceptions
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils.crypto import get_random_string
 from django.views.generic import View
+
 from .forms import RegisterForm
+from .models import User
 
 
 # Create your views here.
@@ -19,9 +21,30 @@ class RegisterView(View):
     def post(self, request):
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
-            print(register_form.cleaned_data)
+            username = register_form.cleaned_data.get('username')
+            user_email = register_form.cleaned_data.get('email')
+            user_password = register_form.cleaned_data.get('password')
+            user: bool = User.objects.filter(username__iexact=username).exists()
+            if user:
+                register_form.add_error('username', 'Username already exists')
 
-        context = {
-            'register_form': register_form
-        }
-        return render(request, 'account_module/register_page.html', context)
+            email_user: bool = User.objects.filter(email__iexact=user_email).exists()
+            if email_user:
+                register_form.add_error('email', 'Email already exists')
+            else:
+                new_user = User(
+                    email=user_email,
+                    email_active_code=get_random_string(72),
+                    is_active=False,
+                    username=username
+                )
+                new_user.set_password(user_password)
+                new_user.save()
+                # todo: send email active code
+                # send_email('فعالسازی حساب کاربری', new_user.email, {'user': new_user}, 'emails/activate_account.html')
+                return redirect(reverse('home_page'))
+
+            context = {
+                'register_form': register_form
+            }
+            return render(request, 'account_module/register_page.html', context)
