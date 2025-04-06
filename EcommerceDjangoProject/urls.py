d9895b7b8540d198e792945c84b912b9daa5f2c8
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-04-06 18:04:54 +0430

    Ecommerce

--- EcommerceDjangoProject/urls.py
+++ EcommerceDjangoProject/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('home_module.urls')),
+                  path('', include('account_module.urls')),
                   path('contact-us/', include('contact_module.urls')),
                   path('products/', include('product_module.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- account_module/views.py
+++ account_module/views.py
@@ -1,3 +1,27 @@
+from logging import raiseExceptions
+
 from django.shortcuts import render
+from django.views.generic import View
+from .forms import RegisterForm
+
 
 # Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+
+        context = {
+            'register_form': register_form
+        }
+        return render(request, 'account_module/register_page.html', context)
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            print(register_form.cleaned_data)
+
+        context = {
+            'register_form': register_form
+        }
+        return render(request, 'account_module/register_page.html', context)
