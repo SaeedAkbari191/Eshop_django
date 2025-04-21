8e5721be9d1c1d2025de7618770cf913fd224897
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-04-21 09:48:47 +0430

    Ecommerce

--- user_panel_module/urls.py
+++ user_panel_module/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.UserPanelDashboardView.as_view(), name='user_panel_dashboard'),
     path('edit-profile/', views.EditProfilePageView.as_view(), name='edit_profile_page'),
+    path('change-pass/', views.ChangePasswordPageView.as_view(), name='change_password_page'),
 ]
