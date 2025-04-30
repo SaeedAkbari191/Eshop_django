bc897958039e76798f10e570ae6aa6062659d396
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-04-30 23:27:21 +0430

    Ecommerce

--- static/js/main.js
+++ static/js/main.js
@@ -172,6 +172,26 @@ function open_close_filter() {
 function AddToOrder(product_id) {
     const count = $('#product-count').val();
     $.get('/order/add-to-order?product_id=' + product_id + '&count=' + count).then(response => {
-        console.log(response);
+        if (response.status === 'SUCCESS') {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+            });
+        }
     })
 }
+
+
